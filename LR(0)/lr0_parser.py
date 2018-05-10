0cc4d99207d59eb983a4c86e9d33384428340ae7
amathebest <amatori.matt@gmail.com>
2018-05-10 15:50:09 +0200

    Started to build logic around new states creation

--- LR(0)/lr0_parser.py
+++ LR(0)/lr0_parser.py
@@ -35,15 +35,36 @@ class lr0Item:
     dot = 0
     isReductionItem = False
 
-    def __init__ (self, production, type):
+    def __init__ (self, production, type, dot, reduct):
         self.production = production
         self.type = type
-        self.dot = 3
-        self.isReductionItem = False
+        self.dot = dot
+        self.isReductionItem = reduct
 
-def create_new_item (production, type):
-    new_state = lr0Item(production, type)
+def create_new_item (production, type, dot, reduct):
+    new_state = lr0Item(production, type, dot, reduct)
     return new_state
+
+def check_item_equality(item_1, item_2):
+    if (item_1.production == item_2.production):
+        if (item_1.type == item_2.type):
+            if (item_1.dot == item_2.dot):
+                if (item_1.isReductionItem == item_2.isReductionItem):
+                    return True
+                else:
+                    return False
+            else:
+                return False
+        else:
+            return False
+    else:
+        return False
+
+def verify_if_reduction(item, dot):
+    if (dot == len(item.production)):
+        return True
+    else:
+        return False
 
 class lr0State:
     name = 0
@@ -57,9 +78,6 @@ class lr0State:
 
     def add_item (self, item):
         self.item_l.append(item)
-
-    # wip: funzione che muove il dot all'interno delle production
-    # def move_dot (self, item):
 
 def create_new_state (name):
     new_state = lr0State(name)
@@ -78,7 +96,7 @@ class transition:
         self.ending_state = e_state
 
 def create_new_transition (name, element, s_state, e_state):
-    new_transition = lr0State(name, element, s_state, e_state)
+    new_transition = transition(name, element, s_state, e_state)
     return new_transition
 
 # variables declaration section
@@ -164,30 +182,62 @@ for production in a_grammar:
 initial_state = create_new_state(state_counter)
 state_counter += 1
 initial_state.isInitialState = True
-item = create_new_item(a_grammar[0], "Kernel")
+item = create_new_item(a_grammar[0], "Kernel", 3, False)
 initial_state.item_l.append(item)
 for kernel_i in initial_state.item_l:
     for production in grammar:
         if kernel_i.production[kernel_i.dot] == production[0][0]:
-            item = create_new_item(production[0], "Closure")
+            if (verify_if_reduction(kernel_i, kernel_i.dot)):
+                item = create_new_item(production[0], "Closure", 3, True)
+            else:
+                item = create_new_item(production[0], "Closure", 3, False)
             if item not in initial_state.item_l:
                 initial_state.item_l.append(item)
 lr0_states.append(initial_state)
 
 # ciclo for che per ogni simbolo dietro al dot crea transizioni verso nuovo stato
-new_symb_transitions = []
+new_symb_transitions = [] # array che contiene i simboli con cui devo muovermi
 for state in lr0_states:
     for item in state.item_l:
-        if item.production[item.dot] not in new_symb_transitions:
-            new_symb_transitions.append(item.production[item.dot])
-        for element in new_symb_transitions:
-            for item in state.item_l:
-                if 
+        if (not item.isReductionItem):
+            if item.production[item.dot] not in new_symb_transitions:
+                new_symb_transitions.append(item.production[item.dot])
+    for element in new_symb_transitions:
+        require_new_state = True
+        destination_state = 0
+        new_state_items = []
+        for item in state.item_l:
+            print(item.production, item.dot)
+            if (item.production[item.dot] == element):
+                new_item = create_new_item(item.production, item.dot+1, item.type, "Reduction" if verify_if_reduction(item, item.dot+1) else "Not-reduction")
+                new_state_items.append(new_item)
+                for state_n in lr0_states: #controllo che non esista un altro stato con lo stesso kernel con require_new_state
+                    for item_n in state_n.item_l:
+                        if (item_n.type == "Kernel"):
+                            if (check_item_equality(item, item_n)):
+                                require_new_state = False
+                            else:
+                                require_new_state = True
+                    if (not require_new_state):
+                        destination_state = state_n.name
+                if (require_new_state):
+                    new_state = create_new_state(state_counter)
+                    state_counter += 1
+                    lr0_states.append(new_state)
+                    for new_state_item in new_state_items:
+                        new_state.add_item(new_state_item)
+                    new_transition = create_new_transition(transition_counter, element, state.name, new_state.name)
+                    transitions.append(new_transition)
+                    transition_counter += 1
+                else:
+                    new_transition = create_new_transition(transition_counter, element, state.name, destination_state)
+                    transitions.append(new_transition)
+                    transition_counter += 1
 
 
 print("\nState " + str(initial_state.name) + ":")
 for element in initial_state.item_l:
-    print(element.production, str(element.dot), element.type)
+    print(element.production, str(element.dot), element.type, "Reduction" if element.isReductionItem else "Not-reduction")
 
 # table Computation
 header = []
@@ -200,7 +250,7 @@ for element in non_terminal_names:
 
 ll1_table = PrettyTable(header)
 total_lenght = len(non_terminal_names) + len(terminal_names)
-table = [["" for x in range(total_lenght)] for y in range(10)] # 10 -> numero di stati, quindi dovrò fare la computazione dell'automa prima
+table = [["" for x in range(total_lenght)] for y in range(10)] # 10 -> numero di stati
 
 for idx_row in range(10):
     for idx_col in range(total_lenght):
