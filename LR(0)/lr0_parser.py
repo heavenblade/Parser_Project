80489755488333572b3535ed86945781a14dc357
amathebest <amatori.matt@gmail.com>
2018-05-11 10:08:09 +0200

    This damn automa creation is driving me crazy. Pt. 1 out of x

--- LR(0)/lr0_parser.py
+++ LR(0)/lr0_parser.py
@@ -10,7 +10,7 @@ import csv
 import numpy
 import first_and_follow_calculation as ffc
 from prettytable import PrettyTable
-
+#------------------------------------------------------------------------------
 class nonTerminal:
     name = ''
     first_l = []
@@ -28,7 +28,7 @@ class nonTerminal:
 
     def add_follow (self, element):
         self.follow_l.append(element)
-
+#------------------------------------------------------------------------------
 class lr0Item:
     production = []
     type = ""
@@ -44,19 +44,10 @@ class lr0Item:
 def create_new_item (production, type, dot, reduct):
     new_state = lr0Item(production, type, dot, reduct)
     return new_state
-
+#------------------------------------------------------------------------------
 def check_item_equality(item_1, item_2):
-    if (item_1.production == item_2.production):
-        if (item_1.type == item_2.type):
-            if (item_1.dot == item_2.dot):
-                if (item_1.isReductionItem == item_2.isReductionItem):
-                    return True
-                else:
-                    return False
-            else:
-                return False
-        else:
-            return False
+    if (item_1.production == item_2.production and item_1.type == item_2.type and item_1.dot == item_2.dot and item_1.isReductionItem == item_2.isReductionItem):
+        return True
     else:
         return False
 
@@ -66,6 +57,9 @@ def verify_if_reduction(item, dot):
     else:
         return False
 
+def print_item(item):
+    print(item.production, item.type, item.dot, item.isReductionItem)
+#------------------------------------------------------------------------------
 class lr0State:
     name = 0
     item_l = []
@@ -83,6 +77,17 @@ def create_new_state (name):
     new_state = lr0State(name)
     return new_state
 
+def apply_closure(state, symbol):
+    if (ffc.isNonTerminal(symbol)):
+        for production in grammar:
+            if (production[0][0] == symbol):
+                if (production[0][3] == "#"):
+                    new_item = create_new_item(production[0], "Closure", 3, "Reduction")
+                else:
+                    new_item = create_new_item(production[0], "Closure", 3, "Reduction" if (3 == len(production[0])) else "Not-reduction")
+                if (new_item not in state.item_l):
+                    state.add_item(new_item)
+#------------------------------------------------------------------------------
 class transition:
     name = 0
     element = ''
@@ -98,7 +103,7 @@ class transition:
 def create_new_transition (name, element, s_state, e_state):
     new_transition = transition(name, element, s_state, e_state)
     return new_transition
-
+#------------------------------------------------------------------------------
 # variables declaration section
 terminal_names = []                                     # strings of terminals
 non_terminal_names = []                                 # just strings
@@ -179,27 +184,21 @@ for production in a_grammar:
     print(production)
 
 # computation of the LR(0) automa
+
+# starting state
 initial_state = create_new_state(state_counter)
 state_counter += 1
 initial_state.isInitialState = True
-item = create_new_item(a_grammar[0], "Kernel", 3, False)
-initial_state.item_l.append(item)
-for kernel_i in initial_state.item_l:
-    for production in grammar:
-        if kernel_i.production[kernel_i.dot] == production[0][0]:
-            if (verify_if_reduction(kernel_i, kernel_i.dot)):
-                item = create_new_item(production[0], "Closure", 3, True)
-            else:
-                item = create_new_item(production[0], "Closure", 3, False)
-            if item not in initial_state.item_l:
-                initial_state.item_l.append(item)
+s_item = create_new_item(a_grammar[0], "Kernel", 3, "Not-reduction")
+initial_state.item_l.append(s_item)
+apply_closure(initial_state, s_item.production[3])
 lr0_states.append(initial_state)
 
 # ciclo for che per ogni simbolo dietro al dot crea transizioni verso nuovo stato
-new_symb_transitions = [] # array che contiene i simboli con cui devo muovermi
 for state in lr0_states:
+    new_symb_transitions = [] # array che contiene i simboli con cui devo muovermi
     for item in state.item_l:
-        if (not item.isReductionItem):
+        if (item.isReductionItem == "Not-reduction"):
             if item.production[item.dot] not in new_symb_transitions:
                 new_symb_transitions.append(item.production[item.dot])
     for element in new_symb_transitions:
@@ -207,9 +206,9 @@ for state in lr0_states:
         destination_state = 0
         new_state_items = []
         for item in state.item_l:
-            print(item.production, item.dot)
             if (item.production[item.dot] == element):
-                new_item = create_new_item(item.production, item.dot+1, item.type, "Reduction" if verify_if_reduction(item, item.dot+1) else "Not-reduction")
+                print("creating new item " + item.production + " " + str(item.dot+1) + "..")
+                new_item = create_new_item(item.production, "Kernel", item.dot+1, "Reduction" if (item.dot+1 == len(item.production)) else "Not-reduction")
                 new_state_items.append(new_item)
                 for state_n in lr0_states: #controllo che non esista un altro stato con lo stesso kernel con require_new_state
                     for item_n in state_n.item_l:
@@ -220,24 +219,30 @@ for state in lr0_states:
                                 require_new_state = True
                     if (not require_new_state):
                         destination_state = state_n.name
-                if (require_new_state):
-                    new_state = create_new_state(state_counter)
-                    state_counter += 1
-                    lr0_states.append(new_state)
-                    for new_state_item in new_state_items:
-                        new_state.add_item(new_state_item)
-                    new_transition = create_new_transition(transition_counter, element, state.name, new_state.name)
-                    transitions.append(new_transition)
-                    transition_counter += 1
-                else:
-                    new_transition = create_new_transition(transition_counter, element, state.name, destination_state)
-                    transitions.append(new_transition)
-                    transition_counter += 1
+        if (require_new_state):
+            new_state = create_new_state(state_counter)
+            state_counter += 1
+            lr0_states.append(new_state)
+            for new_state_item in new_state_items:
+                new_state.add_item(new_state_item)
+                print_item(new_state_item)
+                print("added to state " + str(new_state.name))
+                apply_closure(new_state, new_state_item.production[new_state_item.dot])
+            new_transition = create_new_transition(transition_counter, element, state.name, new_state.name)
+            transitions.append(new_transition)
+            transition_counter += 1
+        else:
+            new_transition = create_new_transition(transition_counter, element, state.name, destination_state)
+            transitions.append(new_transition)
+            transition_counter += 1
 
-
-print("\nState " + str(initial_state.name) + ":")
-for element in initial_state.item_l:
-    print(element.production, str(element.dot), element.type, "Reduction" if element.isReductionItem else "Not-reduction")
+for state in lr0_states:
+    print("\nState " + str(state.name) + ":")
+    for element in state.item_l:
+        print(element.production, ",", element.type, ", Dot is on " + str(element.dot), ", Reduction" if element.isReductionItem else ", Not-reduction")
+for transition in transitions:
+    print("\nTransition " + str(transition.name) + ":")
+    print(transition.name,  transition.element, transition.starting_state, transition.ending_state)
 
 # table Computation
 header = []
@@ -248,7 +253,7 @@ for element in non_terminal_names:
     if element not in header:
         header.append(element)
 
-ll1_table = PrettyTable(header)
+lr0_table = PrettyTable(header)
 total_lenght = len(non_terminal_names) + len(terminal_names)
 table = [["" for x in range(total_lenght)] for y in range(10)] # 10 -> numero di stati
 
@@ -260,10 +265,10 @@ for idx_row in range(10):
             table[idx_row][idx_col] = []
 
 for i in range(10):
-    ll1_table.add_row(table[i])
+    lr0_table.add_row(table[i])
 
-print("\nLL(1) parsing table of the grammar G:")
-print(ll1_table)
+print("\nLR(0) parsing table of the grammar G:")
+print(lr0_table)
 
 #
 #
